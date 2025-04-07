11fcf792fe6d340654674922a7a18cf857eb3adb
Gemcutter <spud.hjc@gmail.com>
2025-04-07 13:20:48 +1000

    update

--- server.js
+++ server.js
@@ -1,24 +1,206 @@
 const express = require('express')
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 app.use(express.static("webpage"))
 
+// define a piece for ease of creation
+class Piece {
+    constructor(type, [row,col], colour) {
+        this.type = type
+        this.location = [row,col]
+        this.colour = colour
+        this.movementMask = []
+    }
+    defineMask() {
+        let pieceMask = getPieceMask()
+        this.movementMask = []
+        let row = -1;
+        for (let i=0;i<64;i++) {
+            let column = i%8
+            if (column==0) {
+                row++
+                this.movementMask.push([])
+            }
+            this.movementMask[row][column] = false
+        }
+        let vectors = {
+            'type':this.type,
+            'colour':this.colour,
+            'location':this.location
+        }
+        if (this.type=="Rook"||this.type=="Queen") {
+            //current location of the piece
+            let startX = this.location[0]
+            let startY = this.location[1]
+            // -row
+            let x = startX
+            let y = startY
+            vectors["-row"] = []
+            let cont = true
+            // while the vector is on the board and has not hit a piece
+            while (x>0&&cont) {
+                x--
+                if (pieceMask[x][y]=="") {
+                    vectors["-row"].push([x,y])
+                }
+                else if (pieceMask[x][y]!=this.colour) {
+                    vectors["-row"].push([x,y])
+                    cont = false
+                }
+                else {
+                    cont = false
+                }
+            }
+            // -col
+            x = startX
+            y = startY
+            vectors["-col"] = []
+            while (y>0&&cont) {
+                y--
+                if (pieceMask[x][y]=="") {
+                    vectors["-col"].push([x,y])
+                }
+                else if (pieceMask[x][y]!=this.colour) {
+                    vectors["-col"].push([x,y])
+                    cont = false
+                }
+                else {
+                    cont = false
+                }
+            }
+            // +row
+            x = startX
+            y = startY
+            vectors["+row"] = []
+            while (x<7&&cont) {
+                x++
+                if (pieceMask[x][y]=="") {
+                    vectors["+row"].push([x,y])
+                }
+                else if (pieceMask[x][y]!=this.colour) {
+                    vectors["+row"].push([x,y])
+                    cont = false
+                }
+                else {
+                    cont = false
+                }
+            }
+            // +col
+            x = startX
+            y = startY
+            vectors["+col"] = []
+            while (y<7&&cont) {
+                y++
+                if (pieceMask[x][y]=="") {
+                    vectors["+col"].push([x,y])
+                }
+                else if (pieceMask[x][y]!=this.colour) {
+                    vectors["+col"].push([x,y])
+                    cont = false
+                }
+                else {
+                    cont = false
+                }
+            }
+        }
+        if (this.type=="Bishop"||this.type=="Queen") {
+
+        }
+        for (let vec in vectors) {
+            for (let i in vec) {
+                
+            }
+        }
+        console.log(vectors)
+        if (this.type=="Knight") {
+
+        }
+        if (this.type=="Pawn") {
+
+        }
+        if (this.type=="King") {
+            
+        }
+        for (let piece in pieceList) {
+            if (piece.colour == this.colour) {
+                
+            }
+        }
+        return this.movementMask
+    }
+}
+// make Connection class to send outputs to when things change
+class Connection {
+    constructor(socket) {
+        this.socket = socket
+    }
+}
+// make datatypes for each class
+let pieceList = []
+let connectionDict = {}
+
+function getPieceMask() {
+    let pieceMask = []
+    for (let i=0;i<8;i++) {
+        pieceMask.push([])
+        for (let j=0;j<8;j++) {
+            pieceMask[i].push("")
+        }
+    }
+    for (let i in pieceList) {
+        pieceMask[pieceList[i].location[0]][pieceList[i].location[1]] = pieceList[i].colour
+    }
+    return pieceMask
+}
+
 io.on('connection', function(socket) {
-    console.log('User '+socket.id+' connected');
-    socket.on('details', function(details) {
-        details.user = socket.id;
-        console.log(details);
-    });
+    console.log('User '+socket.id+' connected')
+    socket.emit("pieces",pieceList)
+    // add connection to the list
+    connectionDict[socket.id] = new Connection(socket)
+
     //Whenever someone disconnects this piece of code executed
     socket.on('disconnect', function () {
-        console.log('User '+socket.id+' disconnected');
+        console.log('User '+socket.id+' disconnected')
+        delete connectionDict[socket.id]
     });
 });
 
-
+function gameSetup() {
+    // clear pieceList
+    pieceList = []
+    // push all the pieces to the list
+    pieceList.push(new Piece('Rook',[0,0],"white"))
+    pieceList.push(new Piece('Knight',[0,1],"white"))
+    pieceList.push(new Piece('Bishop',[0,2],"white"))
+    pieceList.push(new Piece('Queen',[0,3],"white"))
+    pieceList.push(new Piece('King',[0,4],"white"))
+    pieceList.push(new Piece('Bishop',[0,5],"white"))
+    pieceList.push(new Piece('Knight',[0,6],"white"))
+    pieceList.push(new Piece('Rook',[0,7],"white"))
+    for (let i=0;i<8;i++) {
+        pieceList.push(new Piece('Pawn',[1,i],"white"))
+    }
+    pieceList.push(new Piece('Rook',[7,0],"black"))
+    pieceList.push(new Piece('Knight',[7,1],"black"))
+    pieceList.push(new Piece('Bishop',[7,2],"black"))
+    pieceList.push(new Piece('Queen',[7,3],"black"))
+    pieceList.push(new Piece('King',[7,4],"black"))
+    pieceList.push(new Piece('Bishop',[7,5],"black"))
+    pieceList.push(new Piece('Knight',[7,6],"black"))
+    pieceList.push(new Piece('Rook',[7,7],"black"))
+    for (let i=0;i<8;i++) {
+        pieceList.push(new Piece('Pawn',[6,i],"black"))
+    }
+    for (let piece in pieceList) {
+        pieceList[piece].defineMask()
+    }
+}
+gameSetup()
+console.log(getPieceMask())
 http.listen(3000, function() {
-   console.log('listening on *:3000');
-});            
+   console.log('listening on *:3000')
+})
    
